a4e724dbeb2bebbd7007f904f08e7e4bbd8ddacd
Ilias <ilias.dinia@outlook.com>
2025-05-01 17:45:35 +0200

    Created pages and setup the App.tsx file to have them as Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
-import QuizForm from "./components/QuizForm";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ResultsPage from "./pages/ResultsPage";
+import QuizPage from "./components/QuizPage";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <QuizForm />
+      <div className="container py-8">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/quiz/:categoryId/:difficulty" element={<QuizPage />} />
+          <Route path="/results" element={<ResultsPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   </QueryClientProvider>
 );
